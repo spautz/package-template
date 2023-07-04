0a28bc05ce83ea4967a12ae4d0bc2dac8200246f
Steven Pautz <spautz@gmail.com>
2023-07-04 13:49:15 -0400

    Fix e2e test config for CI

--- test-apps/nextjs13-playwright/playwright.config.ts
+++ test-apps/nextjs13-playwright/playwright.config.ts
@@ -44,9 +44,9 @@ export default defineConfig({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'npm run dev',
+    command: 'pnpm run dev',
     url: baseURL,
-    timeout: 10 * 1000,
-    reuseExistingServer: false, // !process.env.CI,
+    timeout: 30 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 });
