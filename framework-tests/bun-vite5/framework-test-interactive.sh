ebed17ae1ec57bb472567aee227cd75291f18fbd
Steven Pautz <spautz@gmail.com>
2024-01-10 19:22:23 -0500

    Dockerfile and in-project scripting setup for bun framework-test

---
+++ framework-tests/bun-vite5/framework-test-interactive.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT_NAME=$(basename "$0")
+echo "### Begin ${THIS_SCRIPT_NAME}"
+
+# Fail if anything in here fails
+set -e
+# Run from the framework-test's directory
+pushd "$(dirname -- "${BASH_SOURCE[0]:-$0}")"
+
+source ../../scripts/helpers/helpers.sh
+
+###################################################################################################
+
+# Ensure the local dir is (reasonly) up-to-date: the base `setup-framework-tests.sh` should have
+# handled all environmental setup already
+../../node_modules/.bin/yalc update
+
+DEFAULT_ARGS="-p 4173:4173 -p 5173:5173"
+
+EXTRA_ARGS=$*
+run_command docker build . -f ./Dockerfile.framework-test --iidfile ./docker-id.txt
+run_command docker run -it $DEFAULT_ARGS $EXTRA_ARGS "$(cat ./docker-id.txt)"  bash
+
+###################################################################################################
+
+popd
+echo "### End ${THIS_SCRIPT_NAME}"
---
+++ framework-tests/bun-vite5/framework-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT_NAME=$(basename "$0")
+echo "### Begin ${THIS_SCRIPT_NAME}"
+
+# Fail if anything in here fails
+set -e
+# Run from the framework-test's directory
+pushd "$(dirname -- "${BASH_SOURCE[0]:-$0}")"
+
+source ../../scripts/helpers/helpers.sh
+
+###################################################################################################
+
+# Ensure the local dir is (reasonly) up-to-date: the base `setup-framework-tests.sh` should have
+# handled all environmental setup already
+../../node_modules/.bin/yalc update
+
+DOCKERFILE_TARGET="${1:-default}"
+
+run_command docker build . -f ./Dockerfile.framework-test --iidfile ./docker-id.txt  --target $DOCKERFILE_TARGET
+echo "DOCKERFILE_TARGET=$DOCKERFILE_TARGET"
+run_command docker run "$(cat ./docker-id.txt)"
+
+###################################################################################################
+
+popd
+echo "### End ${THIS_SCRIPT_NAME}"
