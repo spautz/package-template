02d5fad0f5b379564cda581622e150f865c73cc5
Steven Pautz <spautz@gmail.com>
2024-01-22 21:36:26 -0500

    Autoformat

--- framework-tests/deno-fresh/islands/Counter.tsx
+++ framework-tests/deno-fresh/islands/Counter.tsx
@@ -1,5 +1,5 @@
-import type { Signal } from "@preact/signals";
-import { Button } from "../components/Button.tsx";
+import type { Signal } from '@preact/signals';
+import { Button } from '../components/Button.tsx';
 
 interface CounterProps {
   count: Signal<number>;
@@ -8,9 +8,9 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-8 py-6">
-      <Button onClick={() => props.count.value -= 1}>-1</Button>
+      <Button onClick={() => (props.count.value -= 1)}>-1</Button>
       <p class="text-3xl tabular-nums">{props.count}</p>
-      <Button onClick={() => props.count.value += 1}>+1</Button>
+      <Button onClick={() => (props.count.value += 1)}>+1</Button>
     </div>
   );
 }
