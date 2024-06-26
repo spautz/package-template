ee7aaafed7184d448434e627f7c5f61c16e83a74
Steven Pautz <spautz@gmail.com>
2024-06-25 21:39:19 -0400

    Improve docs website

--- docs-website/src/pages/demo.tsx
+++ docs-website/src/pages/demo.tsx
@@ -2,22 +2,19 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '../components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary')}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
+        <div>
+          <Link className="button button--secondary button--lg" to="/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -34,9 +31,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <main>Hello World</main>
     </Layout>
   );
 }
