3f27f7a161e70fa314048a1634b5bc4289926faf
Steven Pautz <spautz@gmail.com>
2025-06-08 15:08:33 -0400

    Fix scripts to use Stylistic

--- docs-website/docusaurus.config.ts
+++ docs-website/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "spautz's Package-Template Docs",
+  title: 'spautz\'s Package-Template Docs',
   tagline: 'My opinionated template for a npm library repo',
   favicon: 'img/favicon.ico',
 
@@ -48,7 +48,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: "spautz's Package-Template Docs\n",
+      title: 'spautz\'s Package-Template Docs\n',
       // logo: {
       //   alt: 'My Site Logo',
       //   src: 'img/logo.svg',
