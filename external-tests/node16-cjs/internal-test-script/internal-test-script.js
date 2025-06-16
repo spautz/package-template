3862bce3a036eab12515552c63138d6593059b03
Steven Pautz <spautz@gmail.com>
2025-06-16 19:27:42 -0400

    Fix node24-esm test, and add some legacy node16 tests for fun

--- external-tests/node16-cjs/internal-test-script/internal-test-script.js
+++ external-tests/node16-cjs/internal-test-script/internal-test-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { helloWorld } from '@spautz/node-library-template';
+const { helloWorld } = require('@spautz/node-library-template');
 
 if (helloWorld !== 'Hello World!') {
   throw new Error('@spautz/node-library-template did not export "Hello World!"');
