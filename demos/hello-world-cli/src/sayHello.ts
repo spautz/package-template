fff5fbd7e62a231cdbabdd4eefc00993022e5dca
Steven Pautz <spautz@gmail.com>
2025-06-26 19:14:12 -0400

    Upgrade all devDependencies except for Biome (which has some pain with customizing gitignore'd paths in 2.0)

--- demos/hello-world-cli/src/sayHello.ts
+++ demos/hello-world-cli/src/sayHello.ts
@@ -1,7 +1,7 @@
 import { helloWorld } from '@spautz/node-library-template';
 
 const sayHello = () => {
-  // biome-ignore lint/suspicious/noConsoleLog: This is a demo using the console
+  // biome-ignore lint/suspicious/noConsole: This is a demo using the console
   console.log(helloWorld);
 };
 
