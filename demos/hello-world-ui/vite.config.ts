2c12ddf5ebb15bddd830beb7521e413367bf0e6a
Steven Pautz <spautz@gmail.com>
2026-02-26 22:08:15 -0500

    Bump devDependencies

--- demos/hello-world-ui/vite.config.ts
+++ demos/hello-world-ui/vite.config.ts
@@ -1,12 +1,14 @@
 import { withSkipTheBuild } from '@skip-the-build/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig, type UserConfigFnPromise } from 'vite';
 
 import skipTheBuildConfig from '../../skip-the-build.ts';
 
-const viteConfig: UserConfig = await withSkipTheBuild(skipTheBuildConfig, {
-  build: {
-    sourcemap: true,
-  },
-});
+const viteConfig: UserConfigFnPromise = defineConfig(
+  withSkipTheBuild(skipTheBuildConfig, {
+    build: {
+      sourcemap: true,
+    },
+  }),
+);
 
 export default viteConfig;
